8a9152429b5830bae2e57fe05b19d43635cc3592
Vojtech Jasny <vojtech.jasny@gooddata.com>
2016-03-04 22:03:43 +0100

    Added vim and editorconfig

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname $0)
+FILES=".atom .ackrc .bookmarks .gitconfig .npmrc .vim .vimrc .zshrc .zshrc.gdc .editorconfig"
+
+for FILE in $FILES; do
+    if [[ ! -e "$HOME/$FILE" ]]; then
+        ln -s "$SCRIPT_PATH/$FILE" "$HOME/$FILE"
+        echo "Linked $FILE"
+    else
+        echo "Already exists: $FILE"
+    fi
+done
